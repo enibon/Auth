1a74d182845872528e62e34491193f474754e643
Eni Bondarenko <86290670+enibon@users.noreply.github.com>
2022-04-16 11:40:37 +0400

    Update ContactController.php

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -4,8 +4,6 @@ namespace App\Http\Controllers;
 use App\Models\Contact;
 use Illuminate\Http\Request;
 use App\Http\Requests\ContactRequest;
-use Illuminate\Auth\Events\Validated;
-use Illuminate\Support\Facades\Validator;
 
 class ContactController extends Controller
 {
